962d2546a8798279dc0cffb346fdaefbc0215bfb
leonard-soft <leoberfo@gmail.com>
2025-09-22 23:28:08 -0500

    feat: database connection using jpa and hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.jpa.DatabaseTest;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Saidfi es mi rey bonito");
+        
+        DatabaseTest databaseTest = new DatabaseTest();
+        databaseTest.databaseTest();
+
     }
 }
