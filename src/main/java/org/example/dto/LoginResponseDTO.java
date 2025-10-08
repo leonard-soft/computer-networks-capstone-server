a4a8f050de02c6f149f056b0918818cceec07c53
Cristian Galeano <cristian.galeano@jala.university>
2025-10-08 00:31:14 -0500

    feature(TcpService): Login are implemented on TcpService and also LoginResponseDTO

---
+++ src/main/java/org/example/dto/LoginResponseDTO.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+public class LoginResponseDTO{
+    private boolean success;
+    private String message;
+
+    public LoginResponseDTO(boolean success, String message){
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() { return success; }
+    public void setSuccess(boolean success) { this.success = success; }
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+}
