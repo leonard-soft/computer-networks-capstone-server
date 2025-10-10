bf93bd2388144f8808b1379adc2fa1e752acaf82
leonard-soft <leoberfo@gmail.com>
2025-10-09 23:38:59 -0500

    initial commit

--- src/main/java/org/example/tcp/TcpService.java
+++ src/main/java/org/example/tcp/TcpService.java
@@ -1,21 +1,18 @@
 package org.example.tcp;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
 
-import org.example.dto.ConnectedUsersResponseDTO;
 import org.example.dto.LoginResponseDTO;
+import org.example.dto.RegisterResponseDTO;
 import org.example.dto.Request;
 import org.example.dto.RequestPayload;
 import org.example.service.UserService;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 
 public class TcpService {
 
@@ -51,7 +48,7 @@ public class TcpService {
             Socket client = tcpSocket.accept();
 
             System.out.println("Client Connection:" + client.getInetAddress());
-
+            
             /* thread to attend a long quantity of request */
             new Thread(() -> {
                 try 
@@ -75,55 +72,43 @@ public class TcpService {
                                 System.out.println("Request type: " + req.getType());
                                 RequestPayload requestPayload = gson.fromJson(gson.toJson(req.getPayload()), RequestPayload.class);
                                 userService.registerUser(requestPayload);
+
+                                try {
+                                    String message = "register completed succesfully";
+                                    RegisterResponseDTO register = new RegisterResponseDTO(true, message);
+                                    String jsonResponse = gson.toJson(register) + "\n";
+                                    
+                                    OutputStream out = client.getOutputStream();
+                                    out.write(jsonResponse.getBytes());
+                                    out.flush();
+                                } catch (Exception e) {
+                                    RegisterResponseDTO errorResponse = new RegisterResponseDTO(false, "Error: " + e.getMessage());
+                                    String jsonError = gson.toJson(errorResponse) + "\n";
+                                    OutputStream out = client.getOutputStream();
+                                    out.write(jsonError.getBytes());
+                                    out.flush();
+                                }
                                 break;
                         
                             case "login":
                                 try{
-                                    String message;
                                     System.out.println("Request type: " + req.getType());
                                     requestPayload = gson.fromJson(gson.toJson(req.getPayload()), RequestPayload.class);
-                                    if(requestPayload == null || requestPayload.username == null){
-                                        throw new IllegalArgumentException("Invalid payload: Username is missing");
-                                    }
                                     boolean success = userService.loginUser(requestPayload);
-
-                                    if(success){
-                                        message = "Login succesful";
-                                        userService.updateUserState(requestPayload.username, true);
-                                    }else{
-                                        message = "Invalid credentials";
-                                    }
-
+                                    String message = success ? "Login succesful" : "Invalid credentials";
                                     LoginResponseDTO response = new LoginResponseDTO(success, message);
                                     String jsonResponse = gson.toJson(response) + "\n";
+
                                     OutputStream out = client.getOutputStream();
                                     out.write(jsonResponse.getBytes());
                                     out.flush();
-                                }catch(IOException | JsonSyntaxException | IllegalArgumentException e){
+                                }catch(Exception e){
                                     LoginResponseDTO errorResponse = new LoginResponseDTO(false, "Error: " + e.getMessage());
                                     String jsonError = gson.toJson(errorResponse) + "\n";
                                     OutputStream out = client.getOutputStream();
                                     out.write(jsonError.getBytes());
                                     out.flush();
                                 }
-
-                                break;
-                            case "get_online_users":
-                                try {
-                                    System.out.println("Request type: " + req.getType());
-                                    List<String> onlineUsers = userService.getOnlineUsers();
-                                    ConnectedUsersResponseDTO response = new ConnectedUsersResponseDTO(onlineUsers.toArray(new String[0]));
-                                    String jsonResponse = gson.toJson(response) + "\n";
-                                    OutputStream out = client.getOutputStream();
-                                    out.write(jsonResponse.getBytes());
-                                    out.flush();
-                                } catch (Exception e) {
-                                    ConnectedUsersResponseDTO errorResponse = new ConnectedUsersResponseDTO(new String[0]);
-                                    String jsonError = gson.toJson(errorResponse) + "\n";
-                                    OutputStream out = client.getOutputStream();
-                                    out.write(jsonError.getBytes());
-                                    out.flush();
-                                }  
                                 break;
                         
                             default:
