9557b2e1d287a25a6178d6e19c1826f092171a7c
CristianSGaleanoB <stivenbarbosa62@gmail.com>
2025-10-16 22:42:40 -0500

    feat: Logs are implemented on all the project

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -1,23 +1,23 @@
 package org.example.service;
 
-import javax.management.Query;
-
 import org.example.dto.RequestPayload;
 import org.example.entity.Player;
 import org.example.hash.HashMethods;
 import org.example.jpa.JpaUtil;
 import org.example.jpa.Queries;
-
+import org.example.logs.ManageLogs;
 
 import jakarta.persistence.EntityManager;
 
 import java.util.List;
 
 public class UserService {
-    
+
+    private final ManageLogs manageLogs = new ManageLogs();
+
     /**
      * This method do the user register.
-     * 
+     *
      * @param requestPayload data from the client
      */
     public void registerUser(RequestPayload requestPayload) {
@@ -25,23 +25,26 @@ public class UserService {
         EntityManager entityManager = JpaUtil.getEntityManager();
 
         try {
+            manageLogs.saveLog("INFO", "Attempting to register user: " + requestPayload.username);
             entityManager.getTransaction().begin();
 
             entityManager.createNativeQuery(
-                "INSERT INTO game_user (username, password, user_state, user_match_win_quantity) " +
-                "VALUES (?, ?, ?, ?)"
-            )
-            .setParameter(1, requestPayload.username)
-            .setParameter(2, hashMethods.hash(requestPayload.password))
-            .setParameter(3, false) 
-            .setParameter(4, 0)
-            .executeUpdate();
+                            "INSERT INTO game_user (username, password, user_state, user_match_win_quantity) " +
+                                    "VALUES (?, ?, ?, ?)"
+                    )
+                    .setParameter(1, requestPayload.username)
+                    .setParameter(2, hashMethods.hash(requestPayload.password))
+                    .setParameter(3, false)
+                    .setParameter(4, 0)
+                    .executeUpdate();
 
             entityManager.getTransaction().commit();
+            manageLogs.saveLog("INFO", "User registered successfully: " + requestPayload.username);
         } catch (Exception e) {
             if (entityManager.getTransaction().isActive()) {
                 entityManager.getTransaction().rollback();
             }
+            manageLogs.saveLog("ERROR", "Error registering user " + requestPayload.username + ": " + e.getMessage());
             e.printStackTrace();
         } finally {
             entityManager.close();
@@ -53,21 +56,32 @@ public class UserService {
      *
      * @param requestPayload data from the user
      */
-    public boolean loginUser(RequestPayload requestPayload){
+    public boolean loginUser(RequestPayload requestPayload) {
         HashMethods hashMethods = new HashMethods();
-        Queries  queries = new Queries();
+        Queries queries = new Queries();
 
+        manageLogs.saveLog("INFO", "Login attempt for user: " + requestPayload.username);
         Player player = queries.findPlayerByUsername(requestPayload.username);
-        if (player == null) return false;
-        return hashMethods.compareHash(requestPayload.password, player.getPassword());
+        if (player == null) {
+            manageLogs.saveLog("WARN", "Login failed: User not found - " + requestPayload.username);
+            return false;
+        }
+
+        boolean success = hashMethods.compareHash(requestPayload.password, player.getPassword());
+        if (success) {
+            manageLogs.saveLog("INFO", "Login successful for user: " + requestPayload.username);
+        } else {
+            manageLogs.saveLog("WARN", "Login failed: Invalid credentials for user - " + requestPayload.username);
+        }
+        return success;
     }
 
     /**
-     * This method update user state  
+     * This method update user state
      * @param  username, boolean state
      *
      */
-    public void updateUserState(String username, boolean state){
+    public void updateUserState(String username, boolean state) {
         EntityManager em = JpaUtil.getEntityManager();
         try {
             Player player = em.find(Player.class, username);
@@ -76,9 +90,11 @@ public class UserService {
                 em.getTransaction().begin();
                 em.merge(player);
                 em.getTransaction().commit();
+                manageLogs.saveLog("INFO", "User state updated for " + username + " to " + (state ? "online" : "offline"));
             }
         } catch (Exception e) {
             em.getTransaction().rollback();
+            manageLogs.saveLog("ERROR", "Error updating user state for " + username + ": " + e.getMessage());
             throw new RuntimeException("Error updating user state: " + e.getMessage());
         }
     }
@@ -86,7 +102,7 @@ public class UserService {
     /**
      * This method returns all the online users on the DB
      */
-    public List<String> getOnlineUsers(){
+    public List<String> getOnlineUsers() {
         Queries queries = new Queries();
         return queries.getOnlineUsers();
     }
